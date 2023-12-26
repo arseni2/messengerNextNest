4c14780bed2e0442547ec32e8f37dd2830a82506
arseni2 <47722945+arseni2@users.noreply.github.com>
2023-12-26 15:47:05 +0300

    Update auth.controller.ts

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import {Body, Controller, Post} from '@nestjs/common';
 import {UsersDto} from "../users/users.dto";
 import {UsersService} from "../users/users.service";
+import {AuthLoginDto} from "./auth.login.dto";
 
 @Controller('auth')
 export class AuthController {
@@ -9,6 +10,10 @@ export class AuthController {
     ) {
     }
 
+    @Post("signin")
+    signin(@Body() loginDto: AuthLoginDto) {
+        return this.usersService.login(loginDto)
+    }
     @Post("signup")
     signup(@Body() userDto: UsersDto) {
         return this.usersService.createUsers(userDto)
